789b00e1064b6223971107cbff435a3bc474895a
Vadim Lyashenko <terateratera23@gmail.com>
2021-03-26 09:45:07 +1000

    feat: moving tiles

--- src/models/tile.ts
+++ src/models/tile.ts
@@ -1,21 +1,21 @@
 export class Tile extends Phaser.GameObjects.Rectangle {
-    constructor(scene: any, x: number, y: number, width: number, height: number, fillColor: number) {
-        super(scene, x, y, width, height, fillColor);
+  constructor(scene: any, x: number, y: number, width: number, height: number, fillColor: number) {
+    super(scene, x, y, width, height, fillColor);
 
-        var graphics = scene.make.graphics({
-          lineStyle: {
-            width: 10,
-          },
-        });
+    var graphics = scene.make.graphics({
+      lineStyle: {
+        width: 10,
+      },
+    });
 
-        let mask_rect = this;
-        mask_rect.width = mask_rect.width - 1;
-        mask_rect.height = mask_rect.height - 1;
-        graphics.fillRectShape(mask_rect);
-        var mask = graphics.createGeometryMask();
-        this.setMask(mask);
+    // let mask_rect = this;
+    // mask_rect.width = mask_rect.width - 4;
+    // mask_rect.height = mask_rect.height - 4;
+    // graphics.fillRectShape(mask_rect);
+    // var mask = graphics.createGeometryMask();
+    // this.setMask(mask);
 
-        this.setOrigin(0 ,0);
-        scene.add.existing(this);
-    }
+    this.setOrigin(0 ,0);
+    scene.add.existing(this);
+  }
 }
--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -20,12 +20,91 @@ export class GameScene extends Phaser.Scene {
         this.tileGrid[y][x] = this.addTile(x, y);
       }
     }
+
+    // let pointer = this.input.activePointer;
+    let coord_y: number = -1;
+    let coord_x: number = -1;
+
+    let prev_y: number = -1;
+    let prev_x: number = -1;
+
+    this.tileGrid.map(function(row, y) {
+      row.map(function(element, x) {
+        element.setInteractive().on('pointerdown', function(){
+          // console.log(`coord_y:${coord_y} coord_x:${coord_x} element.y:${element.y} element.x:${element.x}`);
+          if (coord_y == -1&& coord_x == -1){
+            element.setStrokeStyle(2, 0x33ee33);
+            coord_y = element.y;
+            coord_x = element.x;
+            prev_y = y;
+            prev_x = x;
+          } else {
+            this.clearStroke();
+            if (this.isNear(coord_y, coord_x, element.y, element.x)) {
+              // console.log('near');
+              this.add.tween({
+                targets: this.tileGrid[prev_y][prev_x],
+                y: element.y,
+                x: element.x, 
+                ease: 'Linear',
+                duration: 300,
+                repeat: 0,
+                yoyo: false,
+                onComplete: () => {
+                }
+              });
+              this.add.tween({
+                targets: element,
+                y: this.tileGrid[prev_y][prev_x].y,
+                x: this.tileGrid[prev_y][prev_x].x,
+                ease: 'Linear',
+                duration: 300,
+                repeat: 0,
+                yoyo: false,
+                onComplete: () => {
+                }
+              });
+              coord_y = -1;
+              coord_x = -1;
+              prev_y = -1;
+              prev_x = -1;
+            } else {
+              element.setStrokeStyle(2, 0x33ee33);
+              coord_y = element.y;
+              coord_x = element.x;
+              prev_y = y;
+              prev_x = x;
+            }
+          // console.log(`prev_y:${prev_y} prev_x:${prev_x}`);
+          }
+          console.log(`y: ${y} x: ${x}`);
+        }, this);
+      }, this);
+    }, this);
   }
 
   private addTile(x: number, y: number): Tile {
-    // let randColor = Math.floor(Math.random()*16777215);
     let randColor = colors[Phaser.Math.RND.between(0, colors.length - 1)];
-    return new Tile(this, x * 75, y * 75, 75, 75, randColor);
+    return new Tile(this, x * 75, y * 75, 71, 71, randColor);
+  }
+
+  private clearStroke(): void {
+    this.tileGrid.map(function(e) {
+      e.map(function(t) {
+        t.setStrokeStyle(0);
+      });
+    });
+  }
+
+  private isNear(coord_y: number, coord_x: number, y: number, x: number): boolean {
+    if ((x == coord_x + 75 || x == coord_x - 75) && y == coord_y) {
+      return true;
+    }
+    else if ((y == coord_y + 75 || y == coord_y - 75) && x == coord_x) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   preload ()
@@ -33,13 +112,8 @@ export class GameScene extends Phaser.Scene {
 
   }
 
-  create ()
-  {
-
-  }
-
   update ()
   {
-
+    // console.log()
   }
 }
